e90c7876dfc02a16ab608860a7ca166257892344
Stefano Crosta <stefano.crosta@gmail.com>
2014-02-09 21:51:12 +0100

    version 1.0 compatible with DjangoCMS 3.0

--- cmsplugin_bootstrap_carousel/models_filer.py
+++ cmsplugin_bootstrap_carousel/models_filer.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.utils.translation import ugettext as _
 
 from filer.fields.image import FilerImageField
-from cms.plugins.link.models import Link
+from djangocms_link.models import Link
 
 class CarouselItem(models.Model):
     carousel = models.ForeignKey(Carousel)
